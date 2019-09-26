0212a033a1bb5ba1b97dfc7eb20f3d42375ee077
Adrien Colonna <adriencolonna@macbook-pro-de-adrien.home>
2019-09-26 19:04:43 +0200

    feat: show events

--- src/Controller/EventsController.php
+++ src/Controller/EventsController.php
@@ -11,12 +11,25 @@ use Symfony\Component\Routing\Annotation\Route;
 class EventsController extends AbstractController
 {
     /**
-     * @Route("/", name="home")
+     * @Route("/", name="home", methods={"GET"})
+     * @Route("/events", name="events.index", methods={"GET"})
      */
     public function index(EventRepository $repo) : Response
     {
 //        $repo = $em->getRepository(Event::class);
         $events = $repo->findAll();
         return $this->render('events/index.html.twig', compact('events'));
+    }
+
+    /**
+     * @Route("/events/{id<[0-9]+>}", name="events.show", methods={"GET"})
+     */
+    public function show(EventRepository $repo, /*$id*/ Event $event)
+    {
+        /*$event = $repo->find($id);
+        if(!$event) {
+            throw $this->createNotFoundException('Event with id #' . $id . ' not found');
+        }*/
+        return $this->render('events/show.html.twig', compact('event'));
     }
 }
